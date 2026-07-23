b0c353de7370c1a60f9c29d5357f58f20c4b020d
Mariana Castañeda Bedoya <93689558+Mrcsbda@users.noreply.github.com>
2026-07-23 17:44:24 -0500

    feat: finish new header

--- src/app/components/app-header/AppHeader.tsx
+++ src/app/components/app-header/AppHeader.tsx
@@ -8,9 +8,11 @@ import { useTranslation } from "react-i18next";
 //STORE
 import { appStore } from "../../store";
 // TYPES
-import { ENUM_LANGUAGE, SECTIONS } from "../../types/data.types";
+import { ENUM_LANGUAGE, ENUM_THEME, SECTIONS } from "../../types/data.types";
 // COMPONENTS
 // CSS
+import { IconMenu3, IconMoon, IconSunLow } from "@tabler/icons-react";
+import { AppText } from "../app-text/AppText";
 import "./AppHeader.css";
 
 export const AppHeader = () => {
@@ -64,12 +66,31 @@ export const AppHeader = () => {
           </div>
           {!isMobile && <b>Mariana Castañeda</b>}
         </div>
-        <div></div>
+        {!isMobile && (
+          <div className="nav__menu-container">
+            {menuOptions.map((option, index) => (
+              <button
+                key={index}
+                onClick={() => {
+                  onScrollToSection(option.id);
+                  setShowMobileMenu(false);
+                }}
+                className="nav__menu-mobile-item"
+              >
+                <AppText
+                  tag="span"
+                  type={
+                    activeSection === option.id ? "nav-item-active" : "nav-item"
+                  }
+                  children={option.label}
+                />
+              </button>
+            ))}
+          </div>
+        )}
         <div className="nav__actions-container">
-          <div
-            className="nav__language-container"
-            data-active={lang.use() === ENUM_LANGUAGE.ES ? "es" : "en"}
-          >
+          {/* CHANGE LANGUAGE */}
+          <div className="nav__language-container" data-active={lang.use()}>
             <span className="nav__language-indicator" />
             <button
               className={`nav__language-btn ${lang.use() === ENUM_LANGUAGE.ES ? "nav__language-btn--active" : ""}`}
@@ -84,200 +105,69 @@ export const AppHeader = () => {
               EN
             </button>
           </div>
-          <div>theme</div>
-          {isMobile && <div>menu</div>}
+          {/* CHANGE THEME */}
+          <div className="nav__theme-container" data-active={theme.use()}>
+            <button
+              className="nav__theme-btn-switch"
+              onClick={() =>
+                changeTheme(
+                  theme.get() === ENUM_THEME.LIGHT
+                    ? ENUM_THEME.DARK
+                    : ENUM_THEME.LIGHT,
+                )
+              }
+            ></button>
+            <IconSunLow
+              stroke={2}
+              width={22}
+              className="nav__theme-icon nav__theme-icon--sun"
+            />
+            <IconMoon
+              stroke={2}
+              width={16}
+              className="nav__theme-icon nav__theme-icon--moon"
+            />
+          </div>
+          {/* MENU ICON MOBILE */}
+          {isMobile && (
+            <button
+              className="nav__menu-mobile-btn"
+              onClick={() => setShowMobileMenu(!showMobileMenu)}
+            >
+              <IconMenu3
+                stroke={2}
+                width={24}
+                height={24}
+                className="nav__menu-mobile-icon"
+              />
+            </button>
+          )}
+        </div>
+        {/* MENU MOBILE */}
+        <div
+          className="nav__menu-mobile-container"
+          data-show={showMobileMenu ? "true" : "false"}
+        >
+          {menuOptions.map((option, index) => (
+            <button
+              key={index}
+              onClick={() => {
+                onScrollToSection(option.id);
+                setShowMobileMenu(false);
+              }}
+              className="nav__menu-mobile-item"
+            >
+              <AppText
+                tag="span"
+                type={
+                  activeSection === option.id ? "nav-item-active" : "nav-item"
+                }
+                children={option.label}
+              />
+            </button>
+          ))}
         </div>
       </section>
     </nav>
-    // <nav className="nav">
-    //   <section className="nav__container" id="header">
-    //     <div className="nav__inner">
-    //       <div className="nav__logo-container">
-    //         <img
-    //           className="nav__logo"
-    //           src="/favicon.svg"
-    //           alt="Logo"
-    //           width={40}
-    //           height={40}
-    //         />
-    //         <AppText
-    //           tag="h2"
-    //           type="fixed"
-    //           children={"MariDev"}
-    //           font="sora"
-    //           fontSize="1.125rem"
-    //           color="text-1"
-    //         />
-    //       </div>
-
-    //       {!isMobile && !isTablet && (
-    //         <ul className="nav__nav-list-container">
-    //           {menuOptions.map((option, index) => (
-    //             <li
-    //               key={index}
-    //               onClick={() => onScrollToSection(option.id)}
-    //               className={`nav__nav-item ${activeSection === option.id ? "nav__nav-item--active" : ""}`}
-    //             >
-    //               <AppText
-    //                 tag="h3"
-    //                 type="body-xl"
-    //                 children={option.label}
-    //                 color={activeSection === option.id ? "accent" : "text-1"}
-    //                 fontWeight="regular"
-    //               />
-    //             </li>
-    //           ))}
-    //         </ul>
-    //       )}
-
-    //       <div className="nav__actions-container">
-    //         {!isMobile && !isTablet && (
-    //           <button
-    //             className="nav__language-container"
-    //             id="themeToggle"
-    //             onClick={() =>
-    //               changeLanguage(
-    //                 lang.get() === ENUM_LANGUAGE.EN
-    //                   ? ENUM_LANGUAGE.ES
-    //                   : ENUM_LANGUAGE.EN,
-    //               )
-    //             }
-    //           >
-    //             <div className="nav__language-btn-desktop-content">
-    //               <span className="material-symbols-rounded">language</span>
-    //               <AppText
-    //                 tag="p"
-    //                 type="fixed"
-    //                 children={lang.get() === ENUM_LANGUAGE.EN ? "ES" : "EN"}
-    //                 color="text-1"
-    //                 fontWeight="regular"
-    //                 fontSize="16px"
-    //               />
-    //             </div>
-    //           </button>
-    //         )}
-
-    //         <button
-    //           className="nav__theme-container"
-    //           id="themeToggle"
-    //           onClick={() =>
-    //             changeTheme(
-    //               theme.get() === ENUM_THEME.LIGHT
-    //                 ? ENUM_THEME.DARK
-    //                 : ENUM_THEME.LIGHT,
-    //             )
-    //           }
-    //         >
-    //           <span id="themeIcon">
-    //             {theme.use() === ENUM_THEME.LIGHT ? "🌙" : "☀️"}
-    //           </span>
-    //         </button>
-
-    //         {(isMobile || isTablet) && (
-    //           <button
-    //             onClick={() => setShowMobileMenu(!showMobileMenu)}
-    //             id="themeToggle"
-    //             className={`nav__menu-mobile-btn ${showMobileMenu ? "nav__menu-mobile-btn--active" : ""}`}
-    //           >
-    //             <span className="nav__menu-mobile-btn--line"></span>
-    //             <span className="nav__menu-mobile-btn--line"></span>
-    //             <span className="nav__menu-mobile-btn--line"></span>
-    //           </button>
-    //         )}
-
-    //         {!isMobile && !isTablet && (
-    //           <AppBtnContained onClick={onDownloadCV}>
-    //             <div className="nav__cv-btn-content">
-    //               <span className="material-symbols-rounded">
-    //                 arrow_downward_alt
-    //               </span>
-    //               <AppText
-    //                 tag="p"
-    //                 type="fixed"
-    //                 children={t("DOWNLOAD_CV")}
-    //                 color="pu-50"
-    //                 fontWeight="regular"
-    //                 fontSize="16px"
-    //               />
-    //             </div>
-    //           </AppBtnContained>
-    //         )}
-    //       </div>
-    //     </div>
-    //   </section>
-
-    //   <section
-    //     className={`nav__menu-mobile-container ${showMobileMenu ? "nav__menu-mobile-container--active" : ""}`}
-    //   >
-    //     <ul className="nav__menu-mobile-list">
-    //       {menuOptions.map((option, index) => (
-    //         <li
-    //           key={index + 1}
-    //           className={`nav__menu-mobile-item ${activeSection === option.id ? "nav__menu-mobile-item--active" : ""}`}
-    //           onClick={() => {
-    //             onScrollToSection(option.id);
-    //             setShowMobileMenu(false);
-    //           }}
-    //         >
-    //           <AppText
-    //             tag="h3"
-    //             type="fixed"
-    //             children={option.label}
-    //             color={activeSection === option.id ? "accent" : "text-1"}
-    //             fontWeight="regular"
-    //             fontSize="16px"
-    //           />
-    //           <span
-    //             className={`material-symbols-rounded nav__menu-mobile-icon ${activeSection === option.id ? "nav__menu-mobile-icon--active" : ""}`}
-    //           >
-    //             chevron_right
-    //           </span>
-    //         </li>
-    //       ))}
-    //     </ul>
-
-    //     <hr className="nav__menu-mobile-divider"></hr>
-
-    //     <div className="nav__menu-mobile-btns-container">
-    //       <AppBtnContained
-    //         onClick={() => {
-    //           changeLanguage(
-    //             lang.get() === ENUM_LANGUAGE.EN
-    //               ? ENUM_LANGUAGE.ES
-    //               : ENUM_LANGUAGE.EN,
-    //           );
-    //         }}
-    //       >
-    //         <div className="nav__language-btn-content">
-    //           <span className="material-symbols-rounded">language</span>
-    //           <AppText
-    //             tag="p"
-    //             type="fixed"
-    //             children={lang.get() === ENUM_LANGUAGE.EN ? "ES" : "EN"}
-    //             color="pu-50"
-    //             fontWeight="regular"
-    //             fontSize="16px"
-    //           />
-    //         </div>
-    //       </AppBtnContained>
-
-    //       <AppBtnOutlined onClick={onDownloadCV}>
-    //         <div className="nav__language-btn-content">
-    //           <span className="material-symbols-rounded">
-    //             arrow_downward_alt
-    //           </span>
-    //           <AppText
-    //             tag="p"
-    //             type="fixed"
-    //             children={t("DOWNLOAD_CV")}
-    //             color="text-1"
-    //             fontWeight="regular"
-    //             fontSize="16px"
-    //           />
-    //         </div>
-    //       </AppBtnOutlined>
-    //     </div>
-    //   </section>
-    // </nav>
   );
 };
--- src/app/store/actions/change-theme.action.ts
+++ src/app/store/actions/change-theme.action.ts
@@ -5,6 +5,7 @@ export const changeTheme = (value: ENUM_THEME) => {
     const { theme, changeFavicon } = appStore()
     document.documentElement.setAttribute('data-theme', value)
     localStorage.setItem('theme', value)
-    theme.set(value);
     changeFavicon(value);
+    theme.set(value);
+
 };
