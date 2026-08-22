3a33a1f3009957b5ee62d65c9b83d24eec17165c
Mariana Castañeda Bedoya <mariana.castanieda@ikualo.com>
2026-08-22 12:48:18 -0500

    feat: implements journey animation step one

--- src/modules/journey/components/journey-animation-one/JourneyAnimationOne.tsx
+++ src/modules/journey/components/journey-animation-one/JourneyAnimationOne.tsx
@@ -1,6 +1,19 @@
+// COMPONENTS
 //CSS
 import "./JourneyAnimationOne.css";
 
 export const JourneyAnimationOne = () => {
-  return <div>Animation 1</div>;
+  return (
+    <div className="journey-animation-one">
+      <div className="journey-animation-one__ring"></div>
+      <div className="journey-animation-one__core">
+        <p className="journey-animation-one__text">‹›</p>
+      </div>
+      <div className="journey-animation-one__spin">
+        <i></i>
+        <i></i>
+        <i></i>
+      </div>
+    </div>
+  );
 };
